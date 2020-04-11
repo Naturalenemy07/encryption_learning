384daa0ab6591832f218c98bae3ec95bbfa698ae
John Caruthers <john.d.caruthers@gmail.com>
2020-04-11 14:57:31 -0400

    Update Kryptos_Cipher_Scrub.py

--- Kryptos_Cipher_Scrub.py
+++ Kryptos_Cipher_Scrub.py
@@ -1,1 +1,5 @@
 #Goal: scrub the Kyptos and Cipher image from CIA website
+#URL to Kyptos Panel/Cipher: https://www.cia.gov/about-cia/headquarters-tour/kryptos/?tab=list-1
+#X-path: //*[@id="kryptos-panel"]
+#X-path to Panel: //*[@id="kryptos-panel"]/img[1]
+#X-path to Cipher: //*[@id="kryptos-panel"]/img[2]
